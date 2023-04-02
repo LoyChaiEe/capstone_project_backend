d1059a768ec0a6640fd97564fa18699bd5510233
LoyChaiEe <celoy001@gmail.com>
2023-04-02 16:54:34 +0800

    added tables,model,seeder for lessons

--- db/migrations/20230402074046-lessons_tables.js
+++ db/migrations/20230402074046-lessons_tables.js
@@ -9,6 +9,22 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    await queryInterface.createTable("characters", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      lesson_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+    });
   },
 
   async down (queryInterface, Sequelize) {
@@ -18,5 +34,6 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+    await queryInterface.dropTable("lessons");
   }
 };
---
+++ db/models/lesson.js
@@ -0,0 +1,31 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Lesson extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    //Asscoiation will be added later
+  }
+  Lesson.init(
+    {
+      lesson_code: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "character",
+      underscored: true,
+      timestamps: false,
+    }
+  );
+  return Lesson;
+};
