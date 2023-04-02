dfb71b3d21dea1dab8087103ad966cf96a3796a4
Nicklye26 <nicklye26@gmail.com>
2023-04-02 20:46:49 +0800

    successfully uploaded profile image to firebase and update it on frontend

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,16 +7,17 @@ class UserController extends BaseController {
   }
   // get specific user via email
   async getOne(req, res) {
-    const { email } = req.params;
+    const { userEmail } = req.params;
     try {
       const user = await this.model.findOne({
-        where: { email_address: email },
+        where: { email_address: userEmail },
       });
       return res.json(user);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
   }
+  // add new user
   async addOne(req, res) {
     const { username, first_name, last_name, email_address, profile_pic_url } =
       req.body;
@@ -39,6 +40,20 @@ class UserController extends BaseController {
         });
         return res.json(user);
       }
+    } catch (err) {
+      return res.status(400).json({ error: true, msg: err });
+    }
+  }
+  // update profile photo URL
+  async updatePhotoURL(req, res) {
+    const { profile_pic_url, email_address } = req.body;
+    try {
+      const user = await this.model.findOne({
+        where: { email_address: email_address },
+      });
+      user.profile_pic_url = profile_pic_url;
+      await user.save({ fields: ["profile_pic_url"] });
+      return res.json(user);
     } catch (err) {
       return res.status(400).json({ error: true, msg: err });
     }
--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -8,8 +8,12 @@ class UsersRouter {
   routes() {
     //Insert routes and controller's api calls here
     router.get("/", this.controller.getAll.bind(this.controller));
-    router.get("/:email", this.controller.getOne.bind(this.controller));
+    router.get("/:userEmail", this.controller.getOne.bind(this.controller));
     router.post("/newUser", this.controller.addOne.bind(this.controller));
+    router.put(
+      "/photoUrl",
+      this.controller.updatePhotoURL.bind(this.controller)
+    );
     return router;
   }
 }
